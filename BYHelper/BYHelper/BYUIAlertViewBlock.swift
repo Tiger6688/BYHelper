36fdf2e99faf373588425cde439df2553383cc05
sgxiang <sgxiang1992@icloud.com>
2014-12-26 18:02:12 +0800

    fix block bug

--- BYHelper/BYHelper/BYUIAlertViewBlock.swift
+++ BYHelper/BYHelper/BYUIAlertViewBlock.swift
@@ -14,20 +14,27 @@ public typealias BYAlertCompleteBlock = ( ( NSInteger) -> Void )?
 
 extension UIAlertView : UIAlertViewDelegate{
     
+    private var __block : BYClosureWrapper<BYAlertCompleteBlock>? {
+        get{
+            return objc_getAssociatedObject(self, &key) as? BYClosureWrapper<BYAlertCompleteBlock>
+        }
+        set{
+            objc_setAssociatedObject(self, &key, newValue, UInt(OBJC_ASSOCIATION_RETAIN_NONATOMIC));
+        }
+    }
+    
     public func alertView(alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int) {
-        var block = objc_getAssociatedObject(self, &key) as? BYClosureWrapper<BYAlertCompleteBlock>
-        block?.call?(buttonIndex)
+        self.__block?.call?(buttonIndex)
     }
     
     public func show(finish : BYAlertCompleteBlock){
         
         if finish != nil{
             self.delegate = self
-            objc_setAssociatedObject(self, &key, BYClosureWrapper(finish!), UInt(OBJC_ASSOCIATION_RETAIN_NONATOMIC));
+            self.__block = BYClosureWrapper(finish)
         }
         self.show()
         
     }
     
 }
-
--- BYHelper/BYHelper/BYUIImagePickerControllerBlock.swift
+++ BYHelper/BYHelper/BYUIImagePickerControllerBlock.swift
@@ -14,26 +14,37 @@ public typealias BYUIImagePickerCompleteBlock = ( (UIImage) -> Void )?
 
 extension UIImagePickerController : UIImagePickerControllerDelegate,UINavigationControllerDelegate{
     
+    
+    private var __block : BYClosureWrapper<BYUIImagePickerCompleteBlock>? {
+        get{
+            return objc_getAssociatedObject(self, &key) as? BYClosureWrapper<BYUIImagePickerCompleteBlock>
+        }
+        set{
+            objc_setAssociatedObject(self, &key, newValue, UInt(OBJC_ASSOCIATION_RETAIN_NONATOMIC));
+        }
+    }
+    
     public func showInViewController( vc : UIViewController , finish : BYUIImagePickerCompleteBlock ){
         
         if(finish != nil){
             self.delegate = self
-            objc_setAssociatedObject(self, &key, BYClosureWrapper(finish!), UInt(OBJC_ASSOCIATION_RETAIN_NONATOMIC))
+            self.__block = BYClosureWrapper(finish)
         }
         self.allowsEditing = true
         
         self.sourceType = .PhotoLibrary
         vc.presentViewController(self, animated: true, completion: nil)
+            
         
     }
     
     public func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]){
         let image: UIImage? = info[UIImagePickerControllerEditedImage] as? UIImage
         if image != nil{
-            var block = objc_getAssociatedObject(self, &key) as? BYClosureWrapper<BYUIImagePickerCompleteBlock>
-            block?.call?(image!)
+            self.__block?.call?(image!)
         }
     }
     
 }
+
 
